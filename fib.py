b4f462cff9deaeb2a359bd62467520403f849a25
hoha <mangocolores@hotmail.com>
2013-08-08 19:55:08 -0500

    Se añade una funcion en fib linea de comandos >>> import nombrearchivosinpy >>> nombrearchivosinpy.nombrefuncion

--- fib.py
+++ fib.py
@@ -5,5 +5,12 @@ def fib (n):
    while b<n:
 	print b,
 	a,b = b,a+b
+'''import fib te ejecuta'''
 
-fib(3)
+def fib2(n):
+    result=[]
+    a,b = 0,1
+    while b<n:
+      result.append(b)
+      a,b = b,a+b
+    return result
